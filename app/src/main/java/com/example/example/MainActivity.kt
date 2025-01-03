555d95ad575461d4f2e9331dad68d9a5184cccdb
prakhar175 <prakhar.k2004@gmail.com>
2025-01-04 00:19:44 +0530

    Files

--- app/src/main/java/com/example/example/MainActivity.kt
+++ app/src/main/java/com/example/example/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -34,7 +29,7 @@ fun App() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "first") {
         composable("first") {
-            FirstScreen(navigationToSecond = {name,age->
+            FirstScreen(navigationToSecond = {name,age->9
                 navController.navigate("second/$name/$age")
             })
         }
